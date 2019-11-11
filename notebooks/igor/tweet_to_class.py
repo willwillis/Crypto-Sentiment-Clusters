d6f78c76a484c25afd76d20c99f990cfacf410ec
stragitech <igoregin@gmail.com>
2019-11-11 13:31:12 -0600

    Made a class for basic tweet data
    created_at, screen_name, and text

---
+++ notebooks/igor/tweet_to_class.py
@@ -0,0 +1,67 @@
+# setting values to not error out
+
+null= 0
+false = False
+true = True
+none = None
+
+#single tweet dict
+tweet = {"contributors":null,"coordinates":null,"created_at":"Sat Oct 05 01:35:59 +0000 2019","display_text_range":[12,26],"entities":
+    {"hashtags":[],"symbols":[],"urls":[],"user_mentions":
+        [
+            {"id":1078767273958100993,"id_str":"1078767273958100993","indices":[0,11],"name":"★ Sara // 23 ★","screen_name":"imsaraawdw"
+            },
+            {"id":1409798257,"id_str":"1409798257","indices":[12,23],"name":"BTS_official","screen_name":"bts_bighit"
+            }
+        ]
+    },
+"favorite_count":0,"favorited":false,"filter_level":"low","geo":null,"id":1180295537636995080,"id_str":"1180295537636995080","in_reply_to_screen_name":"imsaraawdw","in_reply_to_status_id":1180212829678424065,"in_reply_to_status_id_str":"1180212829678424065","in_reply_to_user_id":1078767273958100993,"in_reply_to_user_id_str":"1078767273958100993","is_quote_status":false,"lang":"und","place":null,"quote_count":0,"reply_count":0,"retrieved_on":1570239364.7607939243,"retweet_count":0,"retweeted":false,"source":"<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>","text":"@imsaraawdw @bts_bighit um","timestamp_ms":"1570239359660","truncated":false,"user":
+    {"contributors_enabled":false,"created_at":"Mon Apr 16 20:43:55 +0000 2018","default_profile":true,"default_profile_image":false,"description":null,"favourites_count":3880,"follow_request_sent":null,"followers_count":217,"following":null,"friends_count":660,"geo_enabled":false,"id":985982139710738432,"id_str":"985982139710738432","is_translator":false,"lang":null,"listed_count":0,"location":"daniels house","name":"beth xx","notifications":null,"profile_background_color":"F5F8FA","profile_background_image_url":"","profile_background_image_url_https":"","profile_background_tile":false,"profile_banner_url":"https://pbs.twimg.com/profile_banners/985982139710738432/1565210686","profile_image_url":"http://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_link_color":"1DA1F2","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"protected":false,"screen_name":"gwsbeaufoy","statuses_count":1548,"time_zone":null,"translator_type":"none","url":null,"utc_offset":null,"verified":false
+    }
+}
+
+#list of tweets
+tweets = [
+{"contributors":null,"coordinates":null,"created_at":"Sat Oct 05 01:35:59 +0000 2019","display_text_range":[12,26],"entities":
+    {"hashtags":[],"symbols":[],"urls":[],"user_mentions":
+        [
+            {"id":1078767273958100993,"id_str":"1078767273958100993","indices":[0,11],"name":"★ Sara // 23 ★","screen_name":"imsaraawdw"
+            },
+            {"id":1409798257,"id_str":"1409798257","indices":[12,23],"name":"BTS_official","screen_name":"bts_bighit"
+            }
+        ]
+    },
+"favorite_count":0,"favorited":false,"filter_level":"low","geo":null,"id":1180295537636995080,"id_str":"1180295537636995080","in_reply_to_screen_name":"imsaraawdw","in_reply_to_status_id":1180212829678424065,"in_reply_to_status_id_str":"1180212829678424065","in_reply_to_user_id":1078767273958100993,"in_reply_to_user_id_str":"1078767273958100993","is_quote_status":false,"lang":"und","place":null,"quote_count":0,"reply_count":0,"retrieved_on":1570239364.7607939243,"retweet_count":0,"retweeted":false,"source":"<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>","text":"@imsaraawdw @bts_bighit um","timestamp_ms":"1570239359660","truncated":false,"user":
+    {"contributors_enabled":false,"created_at":"Mon Apr 16 20:43:55 +0000 2018","default_profile":true,"default_profile_image":false,"description":null,"favourites_count":3880,"follow_request_sent":null,"followers_count":217,"following":null,"friends_count":660,"geo_enabled":false,"id":985982139710738432,"id_str":"985982139710738432","is_translator":false,"lang":null,"listed_count":0,"location":"daniels house","name":"beth xx","notifications":null,"profile_background_color":"F5F8FA","profile_background_image_url":"","profile_background_image_url_https":"","profile_background_tile":false,"profile_banner_url":"https://pbs.twimg.com/profile_banners/985982139710738432/1565210686","profile_image_url":"http://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_link_color":"1DA1F2","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"protected":false,"screen_name":"gwsbeaufoy","statuses_count":1548,"time_zone":null,"translator_type":"none","url":null,"utc_offset":null,"verified":false
+    }
+},
+    
+{"contributors":null,"coordinates":null,"created_at":"Sat Oct 05 01:33:32 +0000 2019","entities":{"hashtags":[],"symbols":[],"urls":[{"display_url":"dlvr.it/RFXZDy","expanded_url":"http://dlvr.it/RFXZDy","indices":[82,105],"url":"https://t.co/ifgdr2VusD"}],"user_mentions":[]},"favorite_count":0,"favorited":false,"filter_level":"low","geo":null,"id":1180294921091072000,"id_str":"1180294921091072000","in_reply_to_screen_name":null,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"is_quote_status":false,"lang":"tr","place":null,"possibly_sensitive":false,"quote_count":0,"reply_count":0,"retrieved_on":1570239217.772633791,"retweet_count":0,"retweeted":false,"source":"<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>","text":"Usta Analist İnanılmaz Bitcoin Tahmini İçin Tarih Verdi: 100 Bin, 1 Milyon Dolar… https://t.co/ifgdr2VusD","timestamp_ms":"1570239212664","truncated":false,"user":{"contributors_enabled":false,"created_at":"Fri Jan 04 15:42:24 +0000 2013","default_profile":false,"default_profile_image":false,"description":"Döviz kurları - Dolar Euro TL Ekonomi Haberleri","favourites_count":314,"follow_request_sent":null,"followers_count":1590,"following":null,"friends_count":13,"geo_enabled":false,"id":1060760239,"id_str":"1060760239","is_translator":false,"lang":null,"listed_count":7,"location":"Ankara / Türkiye","name":"Döviz Haberleri","notifications":null,"profile_background_color":"FFFFFF","profile_background_image_url":"http://abs.twimg.com/images/themes/theme1/bg.png","profile_background_image_url_https":"https://abs.twimg.com/images/themes/theme1/bg.png","profile_background_tile":false,"profile_banner_url":"https://pbs.twimg.com/profile_banners/1060760239/1484921350","profile_image_url":"http://pbs.twimg.com/profile_images/822446410138320896/RhmpIg41_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/822446410138320896/RhmpIg41_normal.jpg","profile_link_color":"056839","profile_sidebar_border_color":"FFFFFF","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"protected":false,"screen_name":"dovizhaber","statuses_count":33886,"time_zone":null,"translator_type":"none","url":null,"utc_offset":null,"verified":false}}
+
+
+
+]
+
+#print(tweet)
+
+#print(f"""{tweet["created_at"]} {tweet["text"]} """)
+
+#class of tweet data parameters
+class tweet_data:
+    def __init__(self, time_stamp, user, text):
+        self.time_stamp = time_stamp
+        self.user = user
+        self.text = text
+    def details(self):
+        print(f"{self.time_stamp} {self.user} {self.text}")
+
+tweet_1 = tweet_data(tweet["created_at"], tweet["user"], {tweet["user"]["screen_name"]})
+tweet_2 = tweet_data(tweets[0]["created_at"], {tweets[0]["user"]["screen_name"]}, tweets[0]["text"])
+tweet_3 = tweet_data(tweets[1]["created_at"], {tweets[1]["user"]["screen_name"]}, tweets[1]["text"])
+
+
+#print(tweet_1.time_stamp)
+#print(tweet_1.user)
+#print(tweet_1)
+tweet_2.details()
+tweet_3.details()
