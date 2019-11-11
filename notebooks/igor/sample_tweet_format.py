f00717a527e1b6e64abfa72d8b2d592bb3ef59bd
stragitech <igoregin@gmail.com>
2019-11-10 21:39:00 -0600

    Added sample breakdown of tweet data format

---
+++ notebooks/igor/sample_tweet_format.py
@@ -0,0 +1,18 @@
+#the format is: a {dictionary with dictionary of 'entities':{ that has a list of 'user_mentions':[] } with a dictionary of 'user':{} }
+
+{"contributors":null,"coordinates":null,"created_at":"Sat Oct 05 01:35:59 +0000 2019","display_text_range":[12,26],"entities":
+    {"hashtags":[],"symbols":[],"urls":[],"user_mentions":
+        [
+            {"id":1078767273958100993,"id_str":"1078767273958100993","indices":[0,11],"name":"★ Sara // 23 ★","screen_name":"imsaraawdw"
+            },
+            {"id":1409798257,"id_str":"1409798257","indices":[12,23],"name":"BTS_official","screen_name":"bts_bighit"
+            }
+        ]
+    },
+"favorite_count":0,"favorited":false,"filter_level":"low","geo":null,"id":1180295537636995080,"id_str":"1180295537636995080","in_reply_to_screen_name":"imsaraawdw","in_reply_to_status_id":1180212829678424065,"in_reply_to_status_id_str":"1180212829678424065","in_reply_to_user_id":1078767273958100993,"in_reply_to_user_id_str":"1078767273958100993","is_quote_status":false,"lang":"und","place":null,"quote_count":0,"reply_count":0,"retrieved_on":1570239364.7607939243,"retweet_count":0,"retweeted":false,"source":"<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>","text":"@imsaraawdw @bts_bighit um","timestamp_ms":"1570239359660","truncated":false,"user":
+    {"contributors_enabled":false,"created_at":"Mon Apr 16 20:43:55 +0000 2018","default_profile":true,"default_profile_image":false,"description":null,"favourites_count":3880,"follow_request_sent":null,"followers_count":217,"following":null,"friends_count":660,"geo_enabled":false,"id":985982139710738432,"id_str":"985982139710738432","is_translator":false,"lang":null,"listed_count":0,"location":"daniels house","name":"beth xx","notifications":null,"profile_background_color":"F5F8FA","profile_background_image_url":"","profile_background_image_url_https":"","profile_background_tile":false,"profile_banner_url":"https://pbs.twimg.com/profile_banners/985982139710738432/1565210686","profile_image_url":"http://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/1178361135638159363/ZHC9hnZK_normal.jpg","profile_link_color":"1DA1F2","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"protected":false,"screen_name":"gwsbeaufoy","statuses_count":1548,"time_zone":null,"translator_type":"none","url":null,"utc_offset":null,"verified":false
+    }
+}
+
+#the data for timestamp of the tweet ("created_at":), and text content of the tweet ("text":) 
+# can be accessed from the base layer of the dictionary
